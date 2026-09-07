92fe7584aeb35da702389e264b886d463474f77b
yahyabenarouba15-design <yahyabenarouba15@gmail.com>
2026-09-07 16:48:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import os
 import telebot
 
-TOKEN = "8794061789:AAGKAt9yxIdTIH-YL1lyCXbp3fX_ZplrBz4"
+TOKEN = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "أهلاً بك يا يحيى! البوت يعمل الآن بنجاح تامة.")
+    bot.reply_to(message, "أهلاً بك يا يحيى! البوت يعمل الآن بنجاح تامة 🚀")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
